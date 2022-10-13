f575ea3a7b15ec04b183c8c86f97ea84f631ce22
TripEEE <evan.trippel@gmail.com>
2022-10-12 21:02:50 -0400

    Added protection from cross-site scripting, refetched tweets on submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,32 +4,53 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function () {
+  //FORM SUBMIT HANDLER 
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  $(".tweetForm").submit(function (event) {
+    event.preventDefault()
+    // const str = $(this).serialize()
+
+
+    const formBody = $('#tweet-text').val()
+    const safeHTML = `<p>${escape(formBody)}</p>`;
+    console.log(safeHTML)
+
+
+    if (safeHTML === "") {
+      alert("Error! Please input a string!")
+      return
     }
-  ]
+    if (safeHTML === null) {
+      alert("Error! Your tweet has an invalid value of null!")
+      return
+    }
+    if (safeHTML.length > 140) {
+      alert("Error! Character limit exceeded!")
+      return
+    }
+
+    $.ajax({
+      beforeSend: function (req) {
+        req.setRequestHeader("Access-Control-Allow-Origin", "*")
+      },
+      url: "/tweets",
+      method: "POST",
+      //looks through all tweet-texts in the html, only finds one cuz #, 
+      //we use the index to grab that element, and then return .value
+      data: { text: safeHTML },
+      success: function () { //on a success
+        $('#tweet-text').val("") //resets the textarea to ""
+        loadTweets() //loads existing tweets so no refresh is necessary
+      }
+    })
+    return false
+  })
 
   const createTweetElement = function (tweetObject) {
     //use jquery to attach the values from tweetData into the elements on the HTML page.
@@ -44,7 +65,7 @@ $(document).ready(function () {
         </header>
         <h6>${tweetObject.content.text}</h6>
         <footer class="articleFooter">
-          <p>${tweetObject.created_at}</p>
+          <p>${timeago.format(tweetObject.created_at)}</p>
           <ul>
             <li><i class="fa-solid fa-flag"></i></li>
             <li><i class="fa-sharp fa-solid fa-retweet"></i></li>
@@ -57,13 +78,18 @@ $(document).ready(function () {
   }
 
   const renderTweets = function (tweets) {
-    let tweetElements = []
     for (let tweet of tweets) {
       let newTweet = createTweetElement(tweet)
-      tweetElements.push(newTweet)
+      $(".tweetContainer").prepend(newTweet)
     }
-    $(".tweetContainer").append(...tweetElements)
   }
 
-  renderTweets(data)
+  const loadTweets = function () {
+    $.ajax("/tweets", { method: "GET" })
+      .then(function (JSONResponse) { //JSONResponse is the server database
+        renderTweets(JSONResponse) //renderTweets then calls the server database
+      })
+  }
+
+  loadTweets()
 })
