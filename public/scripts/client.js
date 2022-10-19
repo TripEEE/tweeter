f2812cc76b6e01f7aca3a6cda3a682760bae2885
TripEEE <evan.trippel@gmail.com>
2022-10-18 20:49:20 -0400

    refactor experiments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,58 +6,11 @@
 $(document).ready(function () {
   //FORM SUBMIT HANDLER 
 
-  const escape = function (str) { //safety check
+  const sanitize = function (str) { //safety check
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
-
-  $(".tweetForm").submit(function (event) {
-    event.preventDefault()
-
-    $("#errorMessage").css("display", "none")
-
-    const formBody = $('#tweet-text').val()
-    const safeHTML = `${escape(formBody)}`;
-
-    if (safeHTML === "") {
-      $("#errorMessage").css("display", "block")
-      $("#errorMessage").html("Error! Please input text!")
-      return
-    }
-    if (safeHTML === null) {
-      $("#errorMessage").css("display", "block")
-      $("#errorMessage").html("Error! Your tweet has an invalid value of null!")
-      return
-    }
-    if (safeHTML.length > 140) { //this is currently only alerting when tweet is submitted
-      $("#errorMessage").css("display", "block")
-      $("#errorMessage").html("Error! Your character limit has been exceeded!")
-      return
-    }
-
-    $.ajax({
-      beforeSend: function (req) { //prevents CORS error
-        req.setRequestHeader("Access-Control-Allow-Origin", "*")
-      },
-      url: "/tweets",
-      method: "POST",
-      //looks through all tweet-texts in the html, only finds one cuz #, 
-      //we use the index to grab that element, and then return .value
-      data: { text: safeHTML },
-      success: function (response) { //on a success
-        console.log(response)
-        $('#tweet-text').val("") //resets the textarea to ""
-        // loadAllTweets() //loads existing tweets so no refresh is necessary
-        let newTweet = createTweetElement(response.tweet)
-        $(".tweetContainer").prepend(newTweet)
-      }
-    })
-
-    $("#counter").val(140) //resets counter
-
-    return false
-  })
 
   const createTweetElement = function (tweetObject) {
     //use jquery to attach the values from tweetData into the elements on the HTML page.
@@ -84,20 +37,105 @@ $(document).ready(function () {
     return $newTweet
   }
 
-  const renderTweets = function (tweets) {
-    $(".tweetContainer").empty() //prevents the existing tweets from being duplicated on page refresh
-    for (let tweet of tweets) {
+  const page = {
+    tweetForm: $('.tweetForm'),
+    errorMessage: $('#errorMessage'),
+    tweetText: $('#tweet-text'),
+    counter: $("#counter"),
+    tweetContainer: $('.tweetContainer'),
+
+    renderTweet: (tweet) => {
       let newTweet = createTweetElement(tweet)
-      $(".tweetContainer").prepend(newTweet)
+      page.tweetContainer.prepend(newTweet)
+    },
+
+    renderTweets: (tweets) => {
+      page.tweetContainer.empty() //prevents the existing tweets from being duplicated on page refresh
+      for (let tweet of tweets) {
+        page.renderTweet(tweet)
+      }
+    },
+
+    loadAllTweets: () => {
+      $.ajax("/tweets", { method: "GET" })
+        .then(function (JSONResponse) { //JSONResponse is the server database
+          page.renderTweets(JSONResponse) //renderTweets then calls the server database
+        })
+    },
+
+    clearTweetInput: () => {
+      page.tweetText.val("")
+    },
+
+    clearErrorMessage: () => {
+      page.errorMessage.css("display", "none");
+    },
+
+    setErrorMessage: (message) => {
+      if (message === "") {
+        page.errorMessage.css("display", "none");
+        return;
+      }
+
+      page.errorMessage.css("display", "block")
+      page.errorMessage.html(message);
+    },
+
+    validateInput: (input) => {
+      if (input === "") {
+        page.setErrorMessage("Error! Please input text!")
+        return false
+      }
+      if (input === null) {
+        page.setErrorMessage("Error! Your tweet has an invalid value of null!")
+        return false
+      }
+      if (input.length > 140) { //this is currently only alerting when tweet is submitted
+        page.setErrorMessage("Error! Your character limit has been exceeded!")
+        return false
+      }
+
+      return true
+    },
+
+    onFormSubmit: (event) => {
+      event.preventDefault();
+      page.setErrorMessage("")
+
+      const input = page.tweetText.val()
+      const sanitizedInput = `${sanitize(input)}`;
+      const isInputValid = page.validateInput(sanitizedInput);
+
+      if (!isInputValid) {
+        return;
+      }
+
+      page.counter.val(140)
+
+      $.ajax({
+        beforeSend: function (req) { //prevents CORS error
+          req.setRequestHeader("Access-Control-Allow-Origin", "*")
+        },
+        url: "/tweets",
+        method: "POST",
+        //looks through all tweet-texts in the html, only finds one cuz #, 
+        //we use the index to grab that element, and then return .value
+        data: { text: sanitizedInput },
+        success: function (response) { //on a success
+          page.clearTweetInput()
+          page.renderTweet(response.tweet)
+        }
+      })
+
+
+    },
+
+    init: () => {
+      page.tweetForm.submit(page.onFormSubmit); // bind submit
+      page.loadAllTweets(); // load all tweets
     }
   }
 
-  const loadAllTweets = function () {
-    $.ajax("/tweets", { method: "GET" })
-      .then(function (JSONResponse) { //JSONResponse is the server database
-        renderTweets(JSONResponse) //renderTweets then calls the server database
-      })
-  }
 
-  loadAllTweets()
+  page.init()
 })
